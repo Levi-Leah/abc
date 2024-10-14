200aa1eab5346867f49de3cfa8532f98a02815eb
Levi V <levi@redhat.com>
2024-10-14 12:39:52 +0900

    mods vars.yml

--- terraform-config/variables.tf
+++ terraform-config/variables.tf
@@ -1,9 +1,3 @@
-# Subscription ID to pass to Bash script
-variable "subscription_id" {
-    description = "Azure Subscription ID"
-    default     = ""
-}
-
 # Location for deploying resources
 variable "location" {
     description = "Azure region to deploy resources"
