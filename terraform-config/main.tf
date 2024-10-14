7a4f1eb2f14aec9880c393778824baefb452442f
Levi V <levi@redhat.com>
2024-10-15 00:16:44 +0900

    adds depend

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -61,33 +61,30 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
         }
 }
 
-# # Output the kubeconfig file
-# output "kube_config" {
-#     value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-#     sensitive = true  # Mark this as sensitive because it contains credentials
-# }
-
-# resource "local_file" "kubeconfig" {
-#     content  = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-#     filename = "${path.module}/kubeconfig"
-# }
-
 # Define the resource to generate kubeconfig using Azure CLI
 resource "null_resource" "get_kubeconfig" {
+    depends_on = [
+        azurerm_resource_group.aks_rg,
+        azurerm_virtual_network.aks_vnet,
+        azurerm_subnet.aks_subnet,
+        azurerm_kubernetes_cluster.aks_cluster
+    ]
+
     provisioner "local-exec" {
         command = "az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.aks_cluster_name} --file ${path.module}/kubeconfig --overwrite-existing"
     }
 }
 
-# Verify cluster connectivity using kubectl
-resource "null_resource" "verify_cluster" {
-    provisioner "local-exec" {
-        command = "kubectl --kubeconfig=${path.module}/kubeconfig get nodes"
-        }
-}
+# # Verify cluster connectivity using kubectl
+# resource "null_resource" "verify_cluster" {
+#     provisioner "local-exec" {
+#         command = "kubectl --kubeconfig=${path.module}/kubeconfig get nodes"
+#     }
+# }
 
 # Define the Helm release
 resource "helm_release" "nodejs_app" {
+    depends_on = [null_resource.get_kubeconfig]  # Wait until kubeconfig is generated
     name       = "abc"
     chart      = "../helm-config"
     namespace  = "default"
