30d660c48f9259bfd5031ff98bd52c1bead36080
Levi V <levi@redhat.com>
2024-10-15 00:33:35 +0900

    try more dep

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -61,14 +61,27 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
         }
 }
 
+# Output the kubeconfig file
+output "kube_config" {
+    value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+    sensitive = true  # Mark this as sensitive because it contains credentials
+}
+
+resource "local_file" "kubeconfig" {
+    content  = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+    filename = "${path.module}/kubeconfig"
+}
+
+# Define the Helm provider with the kubeconfig
+provider "helm" {
+    kubernetes {
+        config_path = "${path.module}/kubeconfig"
+    }
+}
+
 # Define the resource to generate kubeconfig using Azure CLI
 resource "null_resource" "get_kubeconfig" {
-    depends_on = [
-        azurerm_resource_group.aks_rg,
-        azurerm_virtual_network.aks_vnet,
-        azurerm_subnet.aks_subnet,
-        azurerm_kubernetes_cluster.aks_cluster
-    ]
+    depends_on = [azurerm_kubernetes_cluster.aks_cluster, local_file.kubeconfig]
 
     provisioner "local-exec" {
         command = "az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.aks_cluster_name} --file ${path.module}/kubeconfig --overwrite-existing"
@@ -84,7 +97,7 @@ resource "null_resource" "get_kubeconfig" {
 
 # Define the Helm release
 resource "helm_release" "nodejs_app" {
-    depends_on = [null_resource.get_kubeconfig]  # Wait until kubeconfig is generated
+    depends_on = depends_on = [azurerm_kubernetes_cluster.aks_cluster, local_file.kubeconfig, null_resource.get_kubeconfig]  # Wait until kubeconfig is generated
     name       = "abc"
     chart      = "../helm-config"
     namespace  = "default"
