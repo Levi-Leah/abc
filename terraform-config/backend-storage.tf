208fbde1c539d7cdbc5ac55641620b644646135b
Levi V <levi@redhat.com>
2024-10-15 04:24:38 +0900

    trying to import storage?

--- terraform-config/backend-storage.tf
+++ terraform-config/backend-storage.tf
@@ -28,6 +28,16 @@ resource "azurerm_storage_account" "tfstate" {
     }
 }
 
+# Backend Configuration
+terraform {
+    backend "azurerm" {
+        resource_group_name   = azurerm_resource_group.tfstate.name
+        # storage_account_name  = azurerm_storage_account.tfstate.name
+        # container_name        = azurerm_storage_container.tfstate.name
+        # key                   = "terraform.tfstate"  # Name of the state file
+    }
+}
+
 # Create a Storage Container for State Files
 resource "azurerm_storage_container" "tfstate" {
     name                  = "tfstate"
