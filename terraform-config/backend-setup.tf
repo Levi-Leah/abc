316f389e1eb7bd89ac9d160e49f7a0d94a90f422
Levi V <levi@redhat.com>
2024-10-17 01:55:34 +0900

    mods backend tf

--- terraform-config/backend-setup.tf
+++ terraform-config/backend-setup.tf
@@ -1,23 +1,31 @@
-# backend-setup.tf (run this first separately)
-provider "azurerm" {
-    features {}
-}
-
-resource "azurerm_resource_group" "state_rg" {
-    name     = "state-storage-rg"
+# Define the resource group for Terraform state storage
+resource "azurerm_resource_group" "tfstate" {
+    name     = "rg-tfstate"
     location = var.location
 }
 
-resource "azurerm_storage_account" "state_sa" {
-    name                     = "statestorageaccount"
-    resource_group_name      = azurerm_resource_group.state_rg.name
-    location                 = azurerm_resource_group.state_rg.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+# Create the storage account for the state file
+resource "azurerm_storage_account" "tfstate_storage" {
+    name                     = "myterraformstorage"
+    resource_group_name       = azurerm_resource_group.tfstate.name
+    location                  = azurerm_resource_group.tfstate.location
+    account_tier              = "Standard"
+    account_replication_type  = "LRS"
 }
 
-resource "azurerm_storage_container" "state_container" {
-    name                  = "tfstate"
-    storage_account_name  = azurerm_storage_account.state_sa.name
-    container_access_type = "private"
+# Create the container for the state file
+resource "azurerm_storage_container" "tfstate_container" {
+    name                  = "terraform-state"
+    storage_account_name   = azurerm_storage_account.tfstate_storage.name
+    container_access_type  = "private"
+}
+
+# Dynamically configure the backend storage
+terraform {
+    backend "azurerm" {
+        resource_group_name   = azurerm_resource_group.tfstate.name
+        storage_account_name  = azurerm_storage_account.tfstate_storage.name
+        container_name        = azurerm_storage_container.tfstate_container.name
+        key                   = "terraform.tfstate"
+    }
 }
