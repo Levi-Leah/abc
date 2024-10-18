4b3b27c045f0d178c7a1562dcabac28c174130ff
Levi V <levi@redhat.com>
2024-10-18 22:18:07 +0900

    latest working

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -1,9 +1,9 @@
 # Configure the backend storage
-# If value names change, they must be manually updated in .github/workflows/github-actions.yml
+# If value names change, they must be manually updated in .storage-check.sh
 terraform {
     backend "azurerm" {
         resource_group_name   = "XYZStorageResourceGroup"
-        storage_account_name  = "xyzstorageaccountunique4"
+        storage_account_name  = "xyzstorageaccountunique5"
         container_name        = "xyztoragecontainer"
         key                   = "terraform.tfstate"
     }
